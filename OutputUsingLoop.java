5aad47ec5a5c924f28acdfb4a5b4a88333610145
saniyabanahatti123-eng <saniyabanahatti123@gmail.com>
2026-03-15 17:46:23 +0530

    added outputUsingLoop

---
+++ OutputUsingLoop.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+import java .util.*;
+
+public class OutputUsingLoop {
+    public static void main(String args[]) {
+        int[] marks = new int[3];
+        marks[0] = 54;
+        marks[1] = 56;
+        marks[2] = 67;
+        for (int i = 0; i<3; i++) {
+           System.out.println(marks[i]);
+        }
+
+    }
+}
