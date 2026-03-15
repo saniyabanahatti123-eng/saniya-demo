ed218e2db4568d98e4161d1ad0c9b477370453d4
saniyabanahatti123-eng <saniyabanahatti123@gmail.com>
2026-03-15 11:39:29 +0530

    deleted unnecessary code

--- arrys.java
+++ arrys.java
@@ -1,13 +1,14 @@
 // Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-import java.util.*;
+import java .util.*;
+
 public class arrys {
     public static void main(String args[]) {
-        int[] marks = new int[3] ;
-        marks[0]=54;
-        marks[1]=56;
-        marks[2]=56;
-        for (int i = 0; i <3; i++) {
-            System.out.println(marks[i]);
-        }
+        int[] marks = new int[3];
+        marks[0]=45;
+        marks[1]=67;
+        marks[2]=78;
+         System.out.println(marks[0]);
+          System.out.println(marks[1]);
+           System.out.println(marks[2]);
     }
 }
