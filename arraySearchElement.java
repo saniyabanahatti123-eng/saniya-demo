81afda37533364fe4e91dc33a5d0bb57e560ffdb
saniyabanahatti123-eng <saniyabanahatti123@gmail.com>
2026-03-16 10:37:23 +0530

    add arrasearch element program

--- arraySearchElement.java
+++ arraySearchElement.java
@@ -1,5 +1,6 @@
 // Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 import java.util.Scanner;
+
 public class arraySearchElement {
     public static void main(String args[]) {
 Scanner sc = new Scanner(System.in);
@@ -8,14 +9,12 @@ int number[] = new int[size];
 for (int i = 0; i<size; i++) {
     number[i]= sc. nextInt();
 }
-for (int i=0;i<size; i++) {
-    number[i]= sc.nextInt();
-}
 int x = sc.nextInt();
 for (int i=0; i<number.length; i++) {
      if (number[i]==x) {
         System.out.println("x Found at index i=" +i);
      }
+     sc.close();
 }
     }
     }
