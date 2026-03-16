8ea8b9d94b254b3fb86fcfe873a54336569a36b1
saniyabanahatti123-eng <saniyabanahatti123@gmail.com>
2026-03-16 10:27:22 +0530

    new dsa code

---
+++ arraySearchElement.java
@@ -0,0 +1,21 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+import java.util.Scanner;
+public class arraySearchElement {
+    public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+int size = sc.nextInt();
+int number[] = new int[size];
+for (int i = 0; i<size; i++) {
+    number[i]= sc. nextInt();
+}
+for (int i=0;i<size; i++) {
+    number[i]= sc.nextInt();
+}
+int x = sc.nextInt();
+for (int i=0; i<number.length; i++) {
+     if (number[i]==x) {
+        System.out.println("x Found at index i=" +i);
+     }
+}
+    }
+    }
