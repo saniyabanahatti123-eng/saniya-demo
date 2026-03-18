2a5d59c56aa8fe0dd450f93e36cc994f9f18549a
saniyabanahatti123-eng <saniyabanahatti123@gmail.com>
2026-03-18 10:10:52 +0530

    string name

---
+++ string/StringName.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class StringName {
+    public static void main(String[] args) {
+        String Firstname = "sanu";
+        String lastname = "banahatti";
+        System.out.println(Firstname + " " + lastname);
+    }
+}
