d02091c3bb2f8ab07d024c5f34071d4786e5950f
saniyabanahatti123-eng <saniyabanahatti123@gmail.com>
2026-03-25 10:28:22 +0530

    add code

---
+++ RecursionSum.java
@@ -0,0 +1,15 @@
+import java .util.*;
+public class RecursionSum {
+public static void Sum (int i,int sum,int n) {
+  if (i==n) {
+    sum+=i;
+    System.out.println(sum);
+    return;
+  }
+    sum=sum+i;
+    Sum(i+1,sum,n);
+}
+public static void main(String[] args) {
+    Sum(1, 0, 5);
+}
+}
