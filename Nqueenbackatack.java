4aa771ea8adae76daba8e22fb336d021f7246083
saniyabanahatti123-eng <saniyabanahatti123@gmail.com>
2026-03-28 14:10:41 +0530

    Nqueen code

---
+++ Nqueenbackatack.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class Nqueenbackatack{
+    public boolean isok(int row ,int col, char [][] board){
+        // check leFt row
+        for (int j=0;j<row;j++ ) {
+            if (board[row][j]=='Q') {
+                return  false;
+            }
+        }
+        // check upper-leFt daigonoal
+        for (int i=row ,j=col; i>=0 && j>=0; i-- ,j--) {
+            if (board[i][j]=='Q') {
+                return false;
+            }
+        }
+        // check lower LeFt daigonal
+        for (int i=0,j=0; i<board.length && j>=0; i++,j--) {
+            if (board[i][j]=='Q') {
+                return false;
+            }
+        }
+            return true;
+        }
+    
+        public void solve(char board[][] , int col) {
+            if (col==board.length) {
+                printboard(board); // store
+                return;
+            }
+            for (int row = 0; row<board.length; row++) {
+                if (isok (row ,col,board)) { //base case
+                    board[row][col] = 'Q'; // add
+                    solve(board,col+1); //recurse(For next column)
+                    board[row][col]='.'; // remove
+                }
+            }
+
+        }
+        public void printboard(char[][] board) {
+            for (int i=0;i<board.length; i++) {
+               for (int j=0; j<board.length; j++) {
+                System.out.print(board[i][j]+""); //print the compleate board
+               } 
+               System.out.println();
+            }
+            System.out.println();
+        }
+        public static void main(String[] args) {
+            int n = 4;
+            char[][] board = new char[n][n];
+                        for (int i=0;i<board.length; i++) {
+               for (int j=0; j<board.length; j++) {
+                board[i][j]='.';
+               }
+            }
+            Nqueenbackatack obj = new
+            Nqueenbackatack(); 
+            obj.solve (board , 0);
+            
+        }
+    }
--- backtracking.java
+++ backtracking.java
@@ -1,19 +1,19 @@
-import java.util.*;
+import java .util.*;
 public class backtracking {
-    public static void PrintPermutation(String str ,String perm, int idx) {
+    public static  void PrintPerm(String str,String perm,int idx) {
 if (str.length()==0) {
-    System.out.println(perm);
-    return;
+  System.out.println(perm); 
+  return; 
 }
-        for (int i = 0; i <  str.length(); i++) {
-            char currchar = str.charAt(i);
-            String newstr = str.substring(0,i) + str.substring(i+1);
-        
-            PrintPermutation(newstr, perm+currchar,idx+1);
-        }
+
+for (int i= 0; i < str.length(); i++) {
+    char currstr = str.charAt(i);
+    String newstr = str.substring(0,i) + str.substring(i+1);
+    PrintPerm(newstr,perm+currstr,idx+1);
+}
     }
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         String str = "ABC";
-        PrintPermutation(str , "" ,0);
+    PrintPerm(str,"",0);
     }
 }
